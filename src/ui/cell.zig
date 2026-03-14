c4c63ddadc3be788e366566991f84c5554785abb
MathRoy12 <matou.roy@gmail.com>
2026-03-13 21:07:05 -0400

    Cell img

--- src/ui/cell.zig
+++ src/ui/cell.zig
@@ -1,6 +1,11 @@
 const rl = @import("raylib");
 const btn = @import("button.zig");
 
-const Cell = struct {
-    value: u8,
+pub const CellValue = enum { One, Two, Three, Four, Five, Six, Seven, eight, Empty, Mine };
+
+fn imgFromCellValue() rl.Image {}
+
+pub const Cell = struct {
+    value: CellValue,
+    button: btn.ImgButton,
 };
