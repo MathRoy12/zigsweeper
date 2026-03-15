c410e581261ba1f7894c12752b734d44507c1f9c
MathRoy12 <matou.roy@gmail.com>
2026-03-14 22:49:18 -0400

    adding cell drawing and reveal + flag logic

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-const btn = @import("ui/button.zig");
+const Cell = @import("ui/cell.zig").Cell;
 
-const width = 1500;
-const heigth = 800;
+const width = 1200;
+const heigth = 640;
 
 const mineSpacing: i32 = 45;
 const padding: i32 = 78;
+
+var grid: [30][16]Cell = [_][16]Cell{[_]Cell{undefined} ** 16} ** 30;
 
 pub fn main() !void {
     rl.initWindow(width, heigth, "zigsweeper");
@@ -15,20 +17,41 @@ pub fn main() !void {
 
     rl.setTargetFPS(60);
 
+    initGrid();
+    try Cell.InitCellText();
+
+    var mouseX: u64 = undefined;
+    var mouseY: u64 = undefined;
+
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.dark_gray);
 
-        for (0..30) |x| {
-            for (0..16) |y| {
-                drawSquare(padding + (mineSpacing * @as(i32, @intCast(x))), padding + (mineSpacing * @as(i32, @intCast(y))));
+        mouseX = @intCast(rl.getMouseX());
+        mouseY = @intCast(rl.getMouseY());
+
+        if (rl.isMouseButtonReleased(rl.MouseButton.left)) {
+            _ = grid[@divTrunc(mouseX, 40)][@divTrunc(mouseY, 40)].Reveal();
+        }
+
+        if (rl.isMouseButtonReleased(rl.MouseButton.right)) {
+            grid[@divTrunc(mouseX, 40)][@divTrunc(mouseY, 40)].Flag();
+        }
+
+        for (0..grid.len) |x| {
+            for (0..grid[x].len) |y| {
+                grid[x][y].Draw();
             }
         }
     }
 }
 
-pub fn drawSquare(x: i32, y: i32) void {
-    rl.drawRectangle(x, y, 40, 40, rl.Color.gray);
+pub fn initGrid() void {
+    for (0..30) |x| {
+        for (0..16) |y| {
+            grid[x][y] = Cell.Init(.Empty, @floatFromInt(x * 40), @floatFromInt(y * 40));
+        }
+    }
 }
--- src/ui/cell.zig
+++ src/ui/cell.zig
@@ -1,11 +1,70 @@
 const rl = @import("raylib");
-const btn = @import("button.zig");
 
-pub const CellValue = enum { One, Two, Three, Four, Five, Six, Seven, eight, Empty, Mine };
+const CellState = enum { Hidden, Revealed, Flaged, WasMine, WrongFlag };
+pub const CellValue = enum { One, Two, Three, Four, Five, Six, Seven, Eight, Mine, Empty };
 
-fn imgFromCellValue() rl.Image {}
+var texts: [14]rl.Texture2D = [_]rl.Texture2D{undefined} ** 14;
+
+const isInitialized = false;
 
 pub const Cell = struct {
+    state: CellState,
     value: CellValue,
-    button: btn.ImgButton,
+
+    _rect: rl.Rectangle,
+
+    pub fn Init(value: CellValue, x: f32, y: f32) Cell {
+        const rect: rl.Rectangle = .{
+            .x = x,
+            .y = y,
+            .height = 40,
+            .width = 40,
+        };
+
+        return Cell{ .state = .Hidden, .value = value, ._rect = rect };
+    }
+
+    pub fn InitCellText() !void {
+        texts[0] = try rl.loadTexture("ressource/img/One.png");
+        texts[1] = try rl.loadTexture("ressource/img/Two.png");
+        texts[2] = try rl.loadTexture("ressource/img/Three.png");
+        texts[3] = try rl.loadTexture("ressource/img/Four.png");
+        texts[4] = try rl.loadTexture("ressource/img/Five.png");
+        texts[5] = try rl.loadTexture("ressource/img/Six.png");
+        texts[6] = try rl.loadTexture("ressource/img/Seven.png");
+        texts[7] = try rl.loadTexture("ressource/img/Eight.png");
+        texts[8] = try rl.loadTexture("ressource/img/Mine.png");
+        texts[9] = try rl.loadTexture("ressource/img/Empty.png");
+        texts[10] = try rl.loadTexture("ressource/img/Cell.png");
+        texts[11] = try rl.loadTexture("ressource/img/Flag.png");
+        texts[12] = try rl.loadTexture("ressource/img/WasMine.png");
+        texts[13] = try rl.loadTexture("ressource/img/WrongFlag.png");
+    }
+
+    fn getText(self: *Cell) rl.Texture2D {
+        return switch (self.state) {
+            .Hidden => texts[10],
+            .Revealed => texts[@intFromEnum(self.value)],
+            .Flaged => texts[11],
+            .WasMine => texts[12],
+            .WrongFlag => texts[13],
+        };
+    }
+
+    pub fn Reveal(self: *Cell) CellValue {
+        if (self.state == .Hidden) {
+            self.state = .Revealed;
+        }
+
+        return self.value;
+    }
+
+    pub fn Flag(self: *Cell) void {
+        if (self.state == .Hidden) self.state = .Flaged;
+    }
+
+    pub fn Draw(self: *Cell) void {
+        const text: rl.Texture2D = self.getText();
+        rl.drawTexture(text, @intFromFloat(@round(self._rect.x)), @intFromFloat(@round(self._rect.y)), rl.Color.white);
+    }
 };
